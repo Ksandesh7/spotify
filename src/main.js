669ae384c8530911ec84879b7b8aafe224386399
Ksandesh7 <117517950+Ksandesh7@users.noreply.github.com>
2024-05-11 19:50:56 +0530

    Tailwind-setup complete

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import './style.css'
 
 document.addEventListener('DOMContentLoaded', ()=> {
-  if(localStorage.getItem('AccessToken')) window.location.href = 'dashboard/dashboard.html';
+  if(localStorage.getItem('accessToken')) window.location.href = 'dashboard/dashboard.html';
   else window.location.href = 'login/login.html';
 }) 
